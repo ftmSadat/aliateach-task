a39cee9b1e69ae6660bfb31fed1a31401312d027
FatemehSadat Mirshamsi <mirshamsi1331@gmail.com>
2025-01-11 22:35:28 +0330

    add pinia to vue

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
 import './assets/style/style.css'
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 import GlobalComponents from './includes/_globals'
 
+const pinia = createPinia()
 const app = createApp(App)
 
 app.use(router)
+app.use(pinia)
 app.use(GlobalComponents)
 
 app.mount('#app')
